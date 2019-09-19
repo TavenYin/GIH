6132ec6f958aa9fec178699817e78f7a4cc391b7
yintianwen7 <yintianwen7@gmail.com>
2019-09-19 23:17:36 +0800

    核心功能初始化

---
+++ setting.js
@@ -0,0 +1,15 @@
+var setting = {
+    "rep_uri":"https://gitee.com/yintianwen7/img-rep.git",
+    "local_folder":"D:\\dev\\git_project\\taven\\img-rep\\",
+    "path": "\\gihtest",
+    "origin_name": "gitee",
+    "label": "master"
+}
+
+function getSetting() {
+    return setting;
+}
+
+module.exports = {
+    getSetting: getSetting
+}
