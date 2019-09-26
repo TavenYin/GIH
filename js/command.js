fcf4af092cccd3190c115e04ca4c73148e90044e
yintianwen7 <yintianwen7@gmail.com>
2019-09-26 23:19:19 +0800

    搞样式... 一头雾水

--- js/command.js
+++ js/command.js
@@ -1,19 +1,15 @@
 const { execSync } = require('child_process');
 function gitCommitAndPush(fileName) {
-    var label = setting.label;
-    var origin_name = setting.origin_name;
-    var rep_uri = setting.rep_uri;
-    var local_folder = setting.local_folder;
+    
 
     // add && commit && push
-    commandExec('git add .', local_folder)
-    commandExec('git commit -m "'+fileName+'"', local_folder)
+    commandExec('git add .', setting.local_folder)
+    commandExec('git commit -m "'+fileName+'"', setting.local_folder)
     var beginTime = +new Date();
-    commandExec('git push -u '+ origin_name + ' ' + label, local_folder)
+    commandExec('git push -u '+ setting.origin_name + ' ' + setting.label, setting.local_folder)
     var endTime = +new Date();
     console.log("git push用时"+(endTime-beginTime)+"ms");
     // https://gitee.com/yintianwen7/img-rep/blob/master/gihtest/3060ADBC-7B2F-487E-986C-6CB55CCE2C4A.jpg
-    document.querySelector("#markdown").value = rep_uri.replace(".git", "/") + "/raw/" + label + "/" + setting.path + "/" + fileName;
 }
 
 function commandExec(command, path) {
--- js/main.js
+++ js/main.js
@@ -11,15 +11,15 @@ function init() {
 function createWindow() {
   // 创建浏览器窗口。
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 780,
+    height: 550,
     webPreferences: {
       nodeIntegration: true
     }
   })
 
   // 加载index.html文件
-  win.loadFile('index.html')
+  win.loadFile('popup.html')
 
   // 打开开发者工具
   win.webContents.openDevTools()
